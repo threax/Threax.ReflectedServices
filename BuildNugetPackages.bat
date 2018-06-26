38f13c64edeb2272510f49f8d1423d28690a5bd7
Andrew Piper <piper.andrew@spcollege.edu>
2018-06-26 09:29:15 -0400

    Added build script.

---
+++ BuildNugetPackages.bat
@@ -0,0 +1,12 @@
+set BASE=%~dp0
+set SEARCH=%BASE%
+set OUT=%BASE%nuget
+mkdir %OUT%
+pushd %BASE%
+rd /s /q src\Threax.ModelGen\bin
+rd /s /q src\Threax.ModelGen\obj
+dotnet build -c Release
+pushd %SEARCH%
+FOR /R %SEARCH% %%I in (Release\*.nupkg) DO move /Y %%I %OUT%
+popd
+popd
